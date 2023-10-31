e2dc37f7dcc1b4fdf857e4a75026e8a8a1d920c8
Grelgn <grelgn@hotmail.com>
2023-10-31 06:33:46 +0300

    Remove console.log() statements

--- src/index.js
+++ src/index.js
@@ -8,20 +8,13 @@ export const todo1 = new ToDo("Default Todo", "2023-10-31", "high", "This is the
 let savedData = JSON.parse(localStorage.getItem('savedData'));
 export let projectArray = savedData;
 
-console.log(JSON.stringify(projectArray));
-console.log(projectArray.length);
-
 if (projectArray.length == 0) {
     project1.addTodo(todo1);
     projectArray.push(project1);
-    console.log("empty");
 }
 
 export let currentProject = projectArray[0];
 
-if(JSON.stringify(projectArray) == JSON.stringify(savedData)) {
-    console.log("same");
-}
 pageLoad();
 newProject();
 newTodo();
