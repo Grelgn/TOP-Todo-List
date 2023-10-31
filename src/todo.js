e90d13a18545d872e44b22188c67e9e6fea84e3b
Grelgn <grelgn@hotmail.com>
2023-10-31 04:59:50 +0300

    Add ability to edit todos

--- src/todo.js
+++ src/todo.js
@@ -15,17 +15,21 @@ export default class ToDo {
     }
 }
 
+const dialog = document.querySelector("#new-todo");
+const todoName = document.querySelector("#todo-name");
+const todoDescription = document.querySelector("#todo-description");
+const todoDueDate = document.querySelector("#todo-due-date");
+const todoPriority = document.querySelector("#todo-priority");
+const addTodo = document.querySelector("#addTodo");
+const b = document.querySelector("#dialog-b");
+
 export function newTodo() {
-    const dialog = document.querySelector("#new-todo");
     const newTodo = document.querySelector(".todo-btn");
     newTodo.addEventListener('click', () => {
         dialog.showModal();
+        b.textContent = "New Todo";
+        addTodo.textContent = "Add New Todo"
     })
-    const todoName = document.querySelector("#todo-name");
-    const todoDescription = document.querySelector("#todo-description");
-    const todoDueDate = document.querySelector("#todo-due-date");
-    const todoPriority = document.querySelector("#todo-priority");
-    const addTodo = document.querySelector("#addTodo");
     addTodo.addEventListener('click', () => {
         currentProject.todoArray.push(new ToDo(todoName.value, todoDueDate.value, todoPriority.value, todoDescription.value));
         pageLoad();
@@ -36,12 +40,32 @@ export function deleteTodo() {
     const button = document.querySelectorAll(".delete-todo");
     let id = 0;
     button.forEach(element => {
-        element.id = 'todo' + id;
+        element.id = 'delete' + id;
         id++;
         element.addEventListener('click', () => {
             let index = element.id.charAt(element.id.length - 1);
             currentProject.todoArray.splice(index, 1);
             pageLoad();
+        })
+    });
+}
+
+export function editTodo() {
+    const button = document.querySelectorAll(".edit-todo");
+    let id = 0;
+    button.forEach(element => {
+        element.id = 'edit' + id;
+        id++;
+        element.addEventListener('click', () => {
+            dialog.showModal();
+            let index = element.id.charAt(element.id.length - 1);
+            b.textContent = "Edit Todo";
+            addTodo.textContent = "Edit Todo"
+            todoName.value = currentProject.todoArray[index].name;
+            todoDescription.value = currentProject.todoArray[index].description;
+            todoDueDate.value = currentProject.todoArray[index].dueDate;
+            todoPriority.value = currentProject.todoArray[index].priority;
+            currentProject.todoArray.splice(index, 1);
         })
     });
 }
